1ec6136d9b9d892830c35928d72915c9beeaaced
raduliano <radulian@me.com>
2022-04-27 11:56:05 +0300

    Initial Commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppApp.swift
+//  AppetizersApp
+//
+//  Created by Octav Radulian on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
